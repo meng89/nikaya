6351e69aa95eee6e1f748c3efee02d9683ebb84f
Chen Meng <ObserverChan@gmail.com>
2016-04-10 11:51:37 +0800

    none

--- make_gh-pages.py
+++ make_gh-pages.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
-import jinja2
+import os
+from jinja2 import Template
 
+from datetime import datetime
+import config
+
+books = []
+
+for filename in sorted(os.listdir(config.BOOKS_DIR)):
+    path = os.path.join(config.BOOKS_DIR, filename)
+    book = {
+        'filename': filename,
+        'path': os.path.join(os.path.split(config.BOOKS_DIR)[1], filename),
+        'size': '{:.1f} MB'.format(os.path.getsize(path) / (1024 * 1024)),
+        'mtime': datetime.fromtimestamp(os.path.getmtime(path)).replace(microsecond=0),
+    }
+
+    books.append(book)
+
+index_str = Template(open('index.html').read()).render(books=books)
+
+open('gh-pages/index.html', 'w').write(index_str)
