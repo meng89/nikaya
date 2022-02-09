4c7a72d7761453e54ae3019da5fa0a251686ec5b
Chen Meng <observerchan@gmail.com>
2022-02-09 23:32:18 +0800

    .

---
+++ pyccc/bookref.py
@@ -0,0 +1,65 @@
+import re
+import utils
+
+P_SA = r"(SA)\.(\d+)"
+P_SN = r"(SN)\.(\d+\.\d+)"
+P_MA = r"(MA)\.(\d+)"
+P_MN = r"(MN)\.(\d+)"
+P_DA = r"(DA)\.(\d+)"
+P_DN = r"(DN)\.(\d+)"
+P_UD = r"(Ud)\.(\d+)"
+P_IT = r"(It)\.(\d+)"
+P_MI = r"(Mi)\.(\d+)"
+P_NI = r"(Ni)\.(\d+)"
+P_PS = r"(Ps)\.(\d+)"
+P_AA = r"(AA)\.(\d+\.\d+)"
+P_AN = r"(AN)\.(\d+\.\d+)"
+
+PATTERNS = [P_SA, P_SN, P_MA, P_MN, P_DA, P_DN, P_UD, P_IT, P_MI, P_NI, P_PS, P_AA, P_AN]
+
+
+class BookRef(object):
+    def __init__(self, pattern: str, bookname: str, num: str):
+        self._pattern = pattern
+        self._BN = bookname
+        self._num = num
+
+    def get_text(self):
+        return self._BN + "." + self._num
+
+    def get_cccurl(self):
+        if self._pattern in [P_SA, P_MA, P_MN, P_DA, P_DN, P_UD, P_IT, P_NI,  P_AA]:
+            return "{}/{}/dm.php?keyword={}".format(utils.CCC_WEBSITE, self._BN, self._num)
+        elif self._pattern in (P_SN, P_AN):
+            return "{}/{}/{}.php?keyword={}".format(utils.CCC_WEBSITE, self._BN, self._BN.lower(), self._num)
+        elif self._pattern in (P_MI, P_NI, P_PS):
+            return "{}/{}/{}{}.htm".format(utils.CCC_WEBSITE, self._BN, self._BN, self._num)
+
+    def __repr__(self):
+        return (f'{self.__class__.__name__}('
+                f'pattern={self._pattern!r}, '
+                f'bookname={self._BN!r}, '
+                f'num={self._num!r})')
+
+
+def get_bookref(y):
+    for p in PATTERNS:
+        m2 = re.match("^{}$".format(p), y)
+        if m2:
+            return BookRef(p, m2.group(1), m2.group(2))
+    assert Exception
+
+
+def split(s):
+    # "[some text SN.1.1, AN.2.1 some text]"
+    list_s = []
+    i = 0
+    for m in re.finditer("|".join(PATTERNS), s):
+        (begin, end) = m.span()
+        list_s.append(s[i:begin])
+        list_s.append(get_bookref(s[begin:end]))
+        i = end
+    if i < len(s):
+        list_s.append(s[i:])
+
+    return list_s
---
+++ pyccc/note.py
@@ -0,0 +1,45 @@
+import re
+import bs4
+import requests.exceptions
+
+import utils
+import bookref
+
+_data = {}
+
+
+def load_global(domain: str):
+    global _data
+    for i in range(100):
+        try:
+            soup = utils.read_url(domain + "/note/note{}.htm".format(i))[0]
+        except requests.exceptions.HTTPError:
+            break
+
+        for div in soup.find_all(name="div", attrs={"id": True}):
+            note_no = re.match(r"^div(\d+)$", div["id"]).group(1)
+
+            listline_list = separate(div.contents)
+
+            _data[note_no] = listline_list
+
+
+def separate(contents):
+    listline_list = []
+    listline = []
+    for one in contents:
+        if isinstance(one, bs4.element.NavigableString):
+            listline.extend(bookref.split(one.get_text()))
+        elif one.name == "br":
+            if listline:
+                listline_list.append(listline)
+                listline = []
+        elif one.name == "a" and "href" in one.attrs.keys():
+            listline.append(utils.Href(text=one.get_text(), href=one["href"], target=one["target"]))
+    if listline:
+        listline_list.append(listline)
+    return listline_list
+
+
+def get():
+    return _data
