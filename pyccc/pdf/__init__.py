c0b35fda57df2471b46508960c2ecddf13bb8c6e
Chen Meng <observerchan@gmail.com>
2022-03-01 07:35:51 +0800

    .

--- pyccc/pdf/__init__.py
+++ pyccc/pdf/__init__.py
@@ -7,4 +7,4 @@ LOCAL_NOTE_KEY_PREFIX = "x"
 
 def note_label(type_, notekey, subnotekey):
     return el(("" if type_ == pyccc.utils.GLOBAL else pyccc.pdf.LOCAL_NOTE_KEY_PREFIX) +
-              str(notekey) + "." + str(subnotekey))
+              str(notekey) + "." + str(subnotekey).replace("(", "").replace(")", ""))
