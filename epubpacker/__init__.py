44658eb85a3f502ce068f38e32af032072e1b0f9
Chen Meng <observerchan@gmail.com>
2022-03-14 23:03:32 +0800

    .

---
+++ epubpacker/__init__.py
@@ -0,0 +1,30 @@
+class Epub(object):
+    def __init__(self):
+        self.toc = _Toc()
+        self.
+        self.root = None
+
+    def write(self):
+        if not self.root:
+            raise EpubError
+
+
+
+class Toc(object):
+    def __init__(self):
+        self.title = None
+        self.href = None
+        self.kids=[]
+
+
+
+class _Spine(object):
+    pass
+
+
+class _Meta(object):
+    pass
+
+
+class EpubError(Exception):
+    pass
