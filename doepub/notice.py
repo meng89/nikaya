afbb4f2a59e6ed84ad4c215980cfbc05bf940382
Chen Meng <observerchan@gmail.com>
2022-03-24 21:54:39 +0800

    .

--- doepub/notice.py
+++ doepub/notice.py
@@ -8,17 +8,17 @@ _doc = """<?xml version="1.0" encoding="UTF-8"?>
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-Hant" lang="zh-Hant">
 <head>
-    <title>制作说明</title>
+    <title>说明</title>
 </head>
 <body>
-<h1>制作说明</h1>
-<p>此佛经来源于<a href="https://agama.buddhason.org">莊春江工作站</a>，一切相关权利归于译者。当前是2022年3月份，仍在订正经文中。
+<h1>说明</h1>
+<p>此佛经来源于<a href="https://agama.buddhason.org">莊春江工作站</a>，一切相关权利归于译者。当前是2022年3月份，译者仍在订正经文中。
 联系译者请前往译者网站留言。</p>
-<p>电纸书制作工具也在开发调整当中，请至制作工具项目主页阅读说明获取制作好的电子书:
+<p>电子书制作工具也在开发调整当中，请至制作程序项目主页阅读说明以获取制作好的电子书：
  <a href="https://github.com/meng89/nikaya">https://github.com/meng89/nikaya</a></p>
-<p>打不开如果打不开上面的链接，请尝试这个云盘链接:
+<p>如果打不开上面的链接，请尝试这个云盘链接：
  <a href="https://www.jianguoyun.com/p/DbVa44QQnbmtChiojLEE">https://www.jianguoyun.com/p/DbVa44QQnbmtChiojLEE</a></p>
-<p>此电子书相关事务请联系我：<a href="mailto:me@chenmeng.org?subject=关于nikaya">me@chenmeng.org</a></p>
+<p>此电子书制作程序的相关问题请联系我：<a href="mailto:me@chenmeng.org">me@chenmeng.org</a></p>
 </body>
 </html>
 """
@@ -28,4 +28,4 @@ def write_notice(epub: epubpacker.Epub, xc: book_public.XC):
     doc_path = "notice.xhtml"
     epub.userfiles[doc_path] = _doc
     epub.spine.append(doc_path)
-    epub.root_toc.append(epubpacker.Toc(xc.c("制作说明"), doc_path))
+    epub.root_toc.append(epubpacker.Toc(xc.c("说明"), doc_path))
--- make_book.py
+++ make_book.py
@@ -34,7 +34,7 @@ def main():
     os.makedirs(books_dir, exist_ok=True)
 
     print_temprootdir()
-    #doepub.sn2epub.make(pyccc.book_public.TC(), temprootdir_td.name, books_dir)
+    doepub.sn2epub.make(pyccc.book_public.TC(), temprootdir_td.name, books_dir)
     doepub.sn2epub.make(pyccc.book_public.SC(), temprootdir_td.name, books_dir)
     dopdf.sneb2pdf.make(pyccc.book_public.TC(), temprootdir_td.name, uc.CONTEXT_BIN_PATH, uc.FONTS_DIR, books_dir)
     dopdf.sneb2pdf.make(pyccc.book_public.SC(), temprootdir_td.name, uc.CONTEXT_BIN_PATH, uc.FONTS_DIR, books_dir)
