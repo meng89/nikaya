3563e38a9b1e0d87fe2f41ac940d894d9b8bf9d8
Chen Meng <observerchan@gmail.com>
2022-03-28 17:42:04 +0800

    .

--- doepub/css.py
+++ doepub/css.py
@@ -24,41 +24,6 @@ p{
     font-weight: 700;
 }
 """
-_tc_font2 = """
-@font-face {
-    font-family: MySerif;
-    src: local('Noto Serif');
-}
-@font-face {
-    font-family: MySerif;
-    src: local('Noto Serif CJK TC');
-    unicode-range: U+4E00-U+9FFF, U+3400-U+4DBF, U+2B740–U+2B81F;
-}
-@font-face {
-    font-family: MySerif;
-    src: local('Noto Serif Tibetan');
-    unicode-range: U+0F00-U+0FFF;
-}
-
-@font-face {
-    font-family: MySans;
-    src: local('Noto Sans');
-}
-@font-face {
-    font-family: MySans;
-    src: local('Noto Sans CJK TC');
-    unicode-range: U+0-2FF;
-}
-
-p{
-    font-family: MySerif;
-}
-.title{
-    font-family: MySans;
-}
-
-"""
-
 
 _sc_font = """
 p{
@@ -73,8 +38,88 @@ p{
 """
 
 
+_tc_font2 = """
+@font-face {
+    font-family: MySerif;
+    src: local('Noto Serif CJK TC');
+    /* unicode-range: U+4E00-U+9FFF, U+3400-U+4DBF, U+2B740–U+2B81F; */
+}
+
+@font-face {
+    font-family: MySerif;
+    src: local('Noto Serif Tibetan');
+    /* unicode-range: U+0F00-U+0FFF; */
+}
+
+@font-face {
+    font-family: MySerif;
+    src: local('Noto Serif');
+}
+
+
+
+@font-face {
+    font-family: MySans;
+    src: local('Noto Sans CJK TC');
+}
+@font-face {
+    font-family: MySans;
+    src: local('Noto Sans');
+}
+
+
+body{
+    font-family: MySerif;
+    font-weight: 400;
+}
+.title{
+    font-family: MySans;
+}
+
+"""
+
+_sc_font2 = """
+@font-face {
+    font-family: MySerif;
+    src: local('Noto Serif CJK SC');
+    /* unicode-range: U+4E00-U+9FFF, U+3400-U+4DBF, U+2B740–U+2B81F; */
+}
+
+@font-face {
+    font-family: MySerif;
+    src: local('Noto Serif Tibetan');
+    /* unicode-range: U+0F00-U+0FFF; */
+}
+
+@font-face {
+    font-family: MySerif;
+    src: local('Noto Serif');
+}
+
+
+
+@font-face {
+    font-family: MySans;
+    src: local('Noto Sans CJK SC');
+}
+@font-face {
+    font-family: MySans;
+    src: local('Noto Sans');
+}
+
+
+body{
+    font-family: MySerif;
+}
+.title{
+    font-family: MySans;
+}
+
+"""
+
+
 font_css = {TC().enlang: _tc_font2,
-            SC().enlang: _sc_font}
+            SC().enlang: _sc_font2}
 
 font_path = {TC().enlang: "_static/tcfont.css",
              SC().enlang: "_static/scfont.css"}
--- test.py
+++ test.py
@@ -7,3 +7,21 @@ from html2image import Html2Image
 
 hti = Html2Image("firefox", browser_executable="/usr/lib64/firefox")
 hti.screenshot(url='https://cn.bing.com', save_as='bing.png')
+
+
+cjk_table = [
+    (0x4E00, 0x9FA5),
+
+]
+
+
+def is_cjk(char):
+    pass
+
+
+def is_latin(char):
+    pass
+
+
+def is_tibetan(char):
+    pass
