a6564b041a438a085b79574f72abe0dd189bf8db
Chen Meng <observerchan@gmail.com>
2022-04-11 23:26:48 +0800

    .

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
-import xl
+from html2image import Html2Image as HtI
+hti = HtI(browser_executable="google-chrome-stable")
+hti.screenshot(html_str=open("cover.xhtml").read(), size=(1600, 2560), save_as="cover.png")
 
-print(xl._unescape("sf&lt;a&amp;bc&gt;ww"))
