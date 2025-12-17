43ab6d1c0fb8423256c04b00f3a3a6be37caa46d
Chen Meng <observerchan@gmail.com>
2025-12-17 18:01:26 +0800

    .

--- hyncdzj_trans2simple.py
+++ hyncdzj_trans2simple.py
@@ -4,11 +4,11 @@ import os
 import config
 from hyncdzj import base
 import hyncdzj_load_from_p5a
-import hyncdzj_book_module
+from  hyncdzj import book_modules
 
 
 def main():
-    for category, ms in hyncdzj_book_module.categories:
+    for category, ms in book_modules.categories:
         for m in ms:
             name, data = hyncdzj_load_from_p5a.load_book_by_module(m)
             data_dir = os.path.join(config.SIMPLE_DOC_DIR, m.info.name)
